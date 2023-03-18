6d9ddc54e6e6773ca1202f0c5614f7e126f4290f
joseevilasio <jose_evilasio@outlook.com>
2023-03-18 22:35:46 +0000

    add route to return all videos and one video

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask
-
+from flask import Flask, g
+from db import conn
+import json
+import sqlite3
 
 app = Flask(__name__)
 
 
+DATABASE = "database.db"
+
+def get_db_connection():
+    conn = sqlite3.connect("database.db").cursor()    
+    return conn
+
+
 @app.route("/")
 def hello():    
-    return "<strong>Hello, World! José</strong>"
+    return "<strong>REST API - MyVideosRepo</strong>"
+
+
+@app.route("/videos")
+def list_all(post_id=None):
+    
+    cursor = get_db_connection() 
+    fields = ("id", "title", "description", "url")
+    results = cursor.execute("SELECT * FROM video;")        
+    return [dict(zip(fields, video)) for video in results]
+
+
+@app.route("/videos/<int:video_id>")
+def list_one(video_id):
+    
+    cursor = get_db_connection() 
+    fields = ("id", "title", "description", "url")
+    results = cursor.execute(f"SELECT * FROM video WHERE id = {video_id};")        
+    return [dict(zip(fields, video)) for video in results]
+
+    
