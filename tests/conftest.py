fdc28bca2ecbae80df36ccaba54ca81aead33cb1
joseevilasio <jose_evilasio@outlook.com>
2023-03-26 21:03:21 +0100

    fix config test database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from sqlalchemy import create_engine
 
 from api.app import create_app
 
@@ -7,3 +8,30 @@ from api.app import create_app
 def app():
     """Instace of Main flask app"""
     return create_app()
+
+
+@pytest.fixture(scope="session")
+def engine():
+    engine = create_engine("sqlite:///:memory:", echo=False)
+
+    # Define o comando SQL para criar a tabela
+    create_table_sql = """\
+        CREATE TABLE if not exists video (
+            id integer PRIMARY KEY AUTOINCREMENT,
+            title varchar UNIQUE NOT NULL,
+            description varchar NOT NULL,
+            url varchar NOT NULL
+        );
+        """
+
+    # Executa o comando SQL para criar a tabela
+    with engine.connect() as conn:
+        conn.exec_driver_sql(create_table_sql)
+
+    # Retorna o engine para utilização nos testes
+    yield engine
+
+    # Fecha a conexão com o engine e limpa o
+    # banco de dados de teste após os testes
+    with engine.connect() as conn:
+        conn.exec_driver_sql("DROP TABLE video")
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,53 @@
 def test_index_positive(client):
-    assert client.get("/").status_code == 200
+    """Test to check if index route is return OK"""
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello, World! MyVideos API" in response.data
 
 
 def test_route_negative(client):
-    assert client.get("/other").status_code == 404
+    """Negative test to check if the non-existent
+    route is returning Not Found"""
+
+    response = client.get("/fake_route")
+    assert response.status_code == 404
+
+
+def test_list_videos_positive(client):
+    """Test to check if list videos route is return OK"""
+
+    response = client.get("/videos")
+    assert response.status_code == 200
+
+
+def test_one_video_positive(engine):
+    """Test to check if one video route is return OK"""
+
+    with engine.connect() as conn:
+        videos = [
+            {
+                "title": """Engenheiro de software ou Programador?
+                com Paulo Silveira""",
+                "description": """Engenheiro de software, pessoa que
+                programa ou dev, o que você é?""",
+                "url": "https://www.youtube.com/watch?v=Gm6U-AxXEQ0",
+            },
+            {
+                "title": "Estrutura de dados com Roberta Arcoverde",
+                "description": """O que são estrutura de dados e como
+                podem ser aplicadas na programação?""",
+                "url": "https://www.youtube.com/watch?v=57VqgNjbzrg",
+            },
+        ]
+
+        conn.exec_driver_sql(
+            """\
+                INSERT INTO video(title, description, url)
+                VALUES (:title, :description, :url);
+                """,
+            videos,
+        )
+        conn.commit()
+        result = conn.exec_driver_sql("SELECT * FROM video;").fetchone()
+        assert result is not None
