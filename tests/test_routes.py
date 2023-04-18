00a07974d1f44ee374eeb7b2fa92656a383adb4e
joseevilasio <jose_evilasio@outlook.com>
2023-04-18 20:48:14 +0100

    add test delete_one_video

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -52,3 +52,23 @@ def test_one_video_positive_data(client):
         assert response_1.status_code == 200
         assert b"Git e Github para iniciantes" in response_1.data
         assert b"https://www.youtube.com/watch?v=8485663" in response_2.data
+
+
+def test_delete_one_video(client):
+    """Test to check if delete one video route is return OK"""
+
+    with get_session() as session:
+
+        add_new_video(VIDEO_FILE)
+
+        result_after = session.exec(select(Video)).first()        
+        assert result_after is not None
+
+        response_delete = client.delete("/videos/1")
+        response_get = client.get("/videos/1")
+
+        result_before = session.exec(select(Video)).first()
+
+        assert result_before == None
+        assert response_delete.status_code == 200
+        assert response_get.status_code == 404
