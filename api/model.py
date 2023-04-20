935e7635378ed9ade84bd3d9284b2ac1c9dab3c9
joseevilasio <jose_evilasio@outlook.com>
2023-04-20 12:22:16 +0100

    fix bug mapper trigger

--- api/model.py
+++ api/model.py
@@ -1,6 +1,6 @@
 from typing import Dict, Optional
 
-from sqlmodel import Field, SQLModel, Relationship
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class Video(SQLModel, table=True):
@@ -10,6 +10,8 @@ class Video(SQLModel, table=True):
     url: str = Field(nullable=False)
 
     category_id: int = Field(foreign_key="category.id")
+
+    category: "Category" = Relationship(back_populates="video")
 
     def to_dict(self) -> Dict[str, str]:
         """Convert a object Video in dict"""
@@ -26,14 +28,14 @@ class Category(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True, index=True)
     title: str = Field(nullable=False)
     color: str = Field(nullable=False)
-    
+
     video: "Video" = Relationship(back_populates="category")
-    
+
     def to_dict(self) -> Dict[str, str]:
         """Convert a object Category in dict"""
 
         return {
             "id": self.id,
             "title": self.title,
-            "color": self.color,            
+            "color": self.color,
         }
