a942e722ae7ec722ff1b46d8d6653dd12f598aaa
joseevilasio <jose_evilasio@outlook.com>
2023-04-20 12:04:39 +0100

    add new table and edit model table Video

--- api/model.py
+++ api/model.py
@@ -1,6 +1,6 @@
 from typing import Dict, Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 
 
 class Video(SQLModel, table=True):
@@ -8,6 +8,8 @@ class Video(SQLModel, table=True):
     title: str = Field(nullable=False)
     description: str = Field(nullable=False)
     url: str = Field(nullable=False)
+
+    category_id: int = Field(foreign_key="category.id")
 
     def to_dict(self) -> Dict[str, str]:
         """Convert a object Video in dict"""
@@ -17,4 +19,21 @@ class Video(SQLModel, table=True):
             "title": self.title,
             "description": self.description,
             "url": self.url,
+        }
+
+
+class Category(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True, index=True)
+    title: str = Field(nullable=False)
+    color: str = Field(nullable=False)
+    
+    video: "Video" = Relationship(back_populates="category")
+    
+    def to_dict(self) -> Dict[str, str]:
+        """Convert a object Category in dict"""
+
+        return {
+            "id": self.id,
+            "title": self.title,
+            "color": self.color,            
         }
