70a6967d563a1bae662c9167b13135202fff71f4
joseevilasio <jose_evilasio@outlook.com>
2023-09-05 16:19:38 +0100

    change database for nosql

--- api/database.py
+++ api/database.py
@@ -1,20 +1,7 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlmodel import Session, create_engine
+from flask_pymongo import PyMongo
 
-from api import model
-
-db = SQLAlchemy()
-
-# db.create_all()
-SQLALCHEMY_DATABASE_URI = "sqlite:///assets/database.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=False)
-model.SQLModel.metadata.create_all(bind=engine)
+mongo = PyMongo()
 
 
-def get_session() -> Session:
-    return Session(engine)
-
-
-def configure(app: Flask):
-    db.init_app(app)
+def configure(app):
+    mongo.init_app(app)
--- api/model.py
+++ api/model.py
@@ -1,42 +1,42 @@
-from typing import Dict, Optional
+# from typing import Dict, Optional
 
-from sqlmodel import Field, Relationship, SQLModel
-
-
-class Video(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True, index=True)
-    title: str = Field(nullable=False)
-    description: str = Field(nullable=False)
-    url: str = Field(nullable=False)
-
-    category_id: int = Field(foreign_key="category.id")
-
-    category: "Category" = Relationship(back_populates="video")
-
-    def to_dict(self) -> Dict[str, str]:
-        """Convert a object Video in dict"""
-
-        return {
-            "id": self.id,
-            "title": self.title,
-            "description": self.description,
-            "url": self.url,
-            "category_id": self.category_id,
-        }
+# from sqlmodel import Field, Relationship, SQLModel
 
 
-class Category(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True, index=True)
-    title: str = Field(nullable=False)
-    color: str = Field(nullable=False)
+# class Video(SQLModel, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True, index=True)
+#     title: str = Field(nullable=False)
+#     description: str = Field(nullable=False)
+#     url: str = Field(nullable=False)
 
-    video: "Video" = Relationship(back_populates="category")
+#     category_id: int = Field(foreign_key="category.id")
 
-    def to_dict(self) -> Dict[str, str]:
-        """Convert a object Category in dict"""
+#     category: "Category" = Relationship(back_populates="video")
 
-        return {
-            "id": self.id,
-            "title": self.title,
-            "color": self.color,
-        }
+#     def to_dict(self) -> Dict[str, str]:
+#         """Convert a object Video in dict"""
+
+#         return {
+#             "id": self.id,
+#             "title": self.title,
+#             "description": self.description,
+#             "url": self.url,
+#             "category_id": self.category_id,
+#         }
+
+
+# class Category(SQLModel, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True, index=True)
+#     title: str = Field(nullable=False)
+#     color: str = Field(nullable=False)
+
+#     video: "Video" = Relationship(back_populates="category")
+
+#     def to_dict(self) -> Dict[str, str]:
+#         """Convert a object Category in dict"""
+
+#         return {
+#             "id": self.id,
+#             "title": self.title,
+#             "color": self.color,
+#         }
