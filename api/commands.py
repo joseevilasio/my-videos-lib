4ac0c016bb89267f8a2f7330a3f6bcc5f89ad201
joseevilasio <jose_evilasio@outlook.com>
2023-09-06 19:10:20 +0100

    update CLI with category options

--- api/commands.py
+++ api/commands.py
@@ -8,13 +8,19 @@ from api.controller import (
     delete_video,
     get_all_videos,
     get_video_by_id,
+    search_video,
     update_video,
+    get_all_category,
+    get_category_by_id,
+    delete_category,
+    update_category,
+    add_new_category
 )
 
 
 @click.group()
 def controller():
-    """Manage MyVideosLIB API"""
+    """Manage >> MyVideosLIB API <<"""
 
 
 @controller.command("show")
@@ -61,6 +67,62 @@ def update(id, data):
 
     result = update_video(int(id), data_dict)
     click.echo(get_video_by_id(result))
+
+
+@controller.command("search")
+@click.argument("search_word", type=click.STRING, required=True)
+def search(search_word):
+    """List all videos match with search"""
+    result = search_video(search_word)
+    click.echo(result)
+
+
+# CATEGORY
+
+@controller.command("show-category")
+def show_category():
+    """List all category"""
+    result = get_all_category()
+    click.echo(result)
+
+
+@controller.command("show-category-by-id")
+@click.argument("id", type=click.STRING, required=True)
+def show_one_category(id):
+    """List only one category by id"""
+    click.echo(get_category_by_id(int(id)))
+
+
+@controller.command("new-category")
+@click.argument("data", required=True)
+def new_category(data):
+    """Add new category on database"""
+
+    with open(data, "r") as data_json:
+        data_dict = json.load(data_json)
+
+    result = add_new_category(data_dict)
+    click.echo(get_category_by_id(result))
+
+
+@controller.command("delete-category")
+@click.argument("id", type=click.STRING, required=True)
+def delete_category(id):
+    """Delete one category by id"""
+    result = delete_category(int(id))
+    click.echo(result)
+
+
+@controller.command("update-category")
+@click.argument("id", type=click.INT, required=True)
+@click.argument("data", required=False)
+def update_category(id, data):
+    """Update category infor on database"""
+    with open(data, "r") as data_json:
+        data_dict = json.load(data_json)
+
+    result = update_category(int(id), data_dict)
+    click.echo(get_category_by_id(result))
 
 
 def configure(app: Flask):
