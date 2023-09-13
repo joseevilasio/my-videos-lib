d5ccc067f0e12fba971a4c6d2744837a782efb7c
joseevilasio <jose_evilasio@outlook.com>
2023-09-13 19:53:27 +0100

    config secret key

--- api/config.py
+++ api/config.py
@@ -1,10 +1,13 @@
 import os
 
-from dynaconf import FlaskDynaconf
+from dynaconf import Dynaconf, FlaskDynaconf
 from flask import Flask
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
+settings = Dynaconf(
+    settings_files=["settings.toml", ".secrets.toml"],
+)
 
 def configure(app: Flask):
     FlaskDynaconf(app, extensions_list="EXTENSIONS", root_path=HERE)
